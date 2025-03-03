6a71b6075b6dbd94217b4d8f5eaebbeb2cf9e156
SaloniGite <salonigite2003@gmail.com>
2025-03-03 23:25:03 +0530

    questions

--- QUESTIONS.py
+++ QUESTIONS.py
@@ -1962,17 +1962,44 @@
 #         return matrix
 
 
-from math import factorial
+# from math import factorial
 
+# class Solution:
+#     def getPermutation(self, n: int, position: int) -> str:
+#         position -= 1
+#         digits = list(range(1,n+1))
+#         result = ''
+#         digit = n - 1
+#         while digit > 0:
+#             chunk_size = factorial(digit)
+#             result += str(digits.pop(position // chunk_size))
+#             position = position % chunk_size
+#             digit -= 1
+#         return result + str(digits[0])
+
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
 class Solution:
-    def getPermutation(self, n: int, position: int) -> str:
-        position -= 1
-        digits = list(range(1,n+1))
-        result = ''
-        digit = n - 1
-        while digit > 0:
-            chunk_size = factorial(digit)
-            result += str(digits.pop(position // chunk_size))
-            position = position % chunk_size
-            digit -= 1
-        return result + str(digits[0])
+    def rotateRight(self, head: Optional[ListNode], k: int) -> Optional[ListNode]:
+        if head==None or head.next==None or k==0:
+            return head
+        l=1
+        curr=head
+        while curr.next:
+            curr=curr.next
+            l+=1
+        r=k%l
+        k=l-r
+        curr.next=head
+        while k>0:
+            curr=curr.next
+            k-=1
+        head=curr.next
+        curr.next = None
+        return head
+
+        
