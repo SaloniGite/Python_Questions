86779c9c3fe590be5535310b517da2ebaa86d3b5
SaloniGite <salonigite2003@gmail.com>
2025-03-05 23:33:28 +0530

    questions

--- QUESTIONS.py
+++ QUESTIONS.py
@@ -2004,24 +2004,53 @@
 
         
 
-from math import factorial
+# from math import factorial
+
+# class Solution:
+#     def uniquePaths(self, m: int, n: int) -> int:
+#         if m == 1: # move straight, one way
+#             return 1
+#         if n == 1: # also move straight
+#             return 1
+
+#         # swap the numbers to avoid using comparisons between them
+#         m, n = min(n,m), max(n,m) 
+
+#         m -= 1 # you need to make M-1 step to reach target
+#         n -= 1 # you need to make N-1 step to reach target
+#         n = n+m # total number of steps you need to make
+
+#         # combination formula
+#         return factorial(n)//factorial(m)//factorial(n-m)
+
+#         # ^ it shows how many combinations we have 
+#         # if choose go down or go right
+
 
 class Solution:
-    def uniquePaths(self, m: int, n: int) -> int:
-        if m == 1: # move straight, one way
-            return 1
-        if n == 1: # also move straight
-            return 1
-
-        # swap the numbers to avoid using comparisons between them
-        m, n = min(n,m), max(n,m) 
-
-        m -= 1 # you need to make M-1 step to reach target
-        n -= 1 # you need to make N-1 step to reach target
-        n = n+m # total number of steps you need to make
-
-        # combination formula
-        return factorial(n)//factorial(m)//factorial(n-m)
-
-        # ^ it shows how many combinations we have 
-        # if choose go down or go right
+    def uniquePathsWithObstacles(self, obstacleGrid: List[List[int]]) -> int:
+        M = len(obstacleGrid)
+        N = len(obstacleGrid[0])
+        
+        if obstacleGrid[0][0] == 1 or obstacleGrid[M-1][N-1] == 1:
+            return 0
+        
+        memo = {}
+        
+        def backtrack(i, j):
+            if i >= M or j >= N:
+                return 0
+            
+            if obstacleGrid[i][j] == 1:
+                return 0
+            
+            if i == M - 1 and j == N - 1:
+                return 1
+            
+            if (i, j) in memo:
+                return memo[(i, j)]
+            
+            memo[(i, j)] = backtrack(i + 1, j) + backtrack(i, j + 1)
+            return memo[(i, j)]
+        
+        return backtrack(0, 0)
