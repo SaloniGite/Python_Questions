a5167a90815f4d3d3093f6a7d0b8fab3a891b478
SaloniGite <salonigite2003@gmail.com>
2025-03-08 23:14:58 +0530

    questions

--- QUESTIONS.py
+++ QUESTIONS.py
@@ -2075,24 +2075,39 @@
     
        
 
-class Solution:
-    def isNumber(self, s: str) -> bool:
-        dfa = [{'space': 0, 'sign': 1, 'digit': 2, '.': 3}, #state 0 - leading space
-               {'digit': 2, '.': 3},                        #state 1 - sign
-               {'digit': 2, '.': 4, 'e': 5, 'space': 8},    #state 2 - digit (terminal)
-               {'digit': 4},                                #state 3 - dot
-               {'digit': 4, 'e': 5, 'space': 8},            #state 4 - digit post dot (terminal)
-               {'sign': 6, 'digit': 7},                     #state 5 - exponential 
-               {'digit': 7},                                #state 6 - sign post exponential 
-               {'digit': 7, 'space': 8},                    #state 7 - digit post exponential (terminal)
-               {'space': 8}                                 #state 8 - trailing space (terminal)
-              ]
+# class Solution:
+#     def isNumber(self, s: str) -> bool:
+#         dfa = [{'space': 0, 'sign': 1, 'digit': 2, '.': 3}, #state 0 - leading space
+#                {'digit': 2, '.': 3},                        #state 1 - sign
+#                {'digit': 2, '.': 4, 'e': 5, 'space': 8},    #state 2 - digit (terminal)
+#                {'digit': 4},                                #state 3 - dot
+#                {'digit': 4, 'e': 5, 'space': 8},            #state 4 - digit post dot (terminal)
+#                {'sign': 6, 'digit': 7},                     #state 5 - exponential 
+#                {'digit': 7},                                #state 6 - sign post exponential 
+#                {'digit': 7, 'space': 8},                    #state 7 - digit post exponential (terminal)
+#                {'space': 8}                                 #state 8 - trailing space (terminal)
+#               ]
         
-        state = 0
-        for c in s.lower(): 
-            if c in "0123456789": c = "digit"
-            elif c == " ":  c = "space"
-            elif c in "+-": c = "sign"
-            if c not in dfa[state]: return False 
-            state = dfa[state][c]
-        return state in [2, 4, 7, 8]
+#         state = 0
+#         for c in s.lower(): 
+#             if c in "0123456789": c = "digit"
+#             elif c == " ":  c = "space"
+#             elif c in "+-": c = "sign"
+#             if c not in dfa[state]: return False 
+#             state = dfa[state][c]
+#         return state in [2, 4, 7, 8]
+
+
+class Solution:
+    def plusOne(self, digits: List[int]) -> List[int]:
+
+        for i in range(len(digits) - 1, -1, -1):
+
+            if digits[i] + 1 != 10:
+                digits[i] += 1
+                return digits
+            
+            digits[i] = 0
+
+            if i == 0:
+                return [1] + digits
