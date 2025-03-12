a54d61f2d7447abdd3b08621eb0b4ae098f8f7d3
SaloniGite <salonigite2003@gmail.com>
2025-03-12 22:58:34 +0530

    questions

--- QUESTIONS.py
+++ QUESTIONS.py
@@ -2132,95 +2132,111 @@
 #     return ''.join(reversed(s))
 
 
-class Solution:   
-    def fullJustify(self,words, maxWidth):
+# class Solution:   
+#     def fullJustify(self,words, maxWidth):
     
-        res = []
+#         res = []
     
-        current_words = []
+#         current_words = []
     
-        current_length = 0  # sum of the lengths of words (no spaces)
+#         current_length = 0  # sum of the lengths of words (no spaces)
 
 
     
-        for w in words:
+#         for w in words:
     
-            # If adding this word plus (at least one space per gap) exceeds maxWidth
+#             # If adding this word plus (at least one space per gap) exceeds maxWidth
     
-            if current_words and current_length + len(w) + len(current_words) > maxWidth:
+#             if current_words and current_length + len(w) + len(current_words) > maxWidth:
     
-                # Justify the current line
+#                 # Justify the current line
     
-                total_spaces = maxWidth - current_length
+#                 total_spaces = maxWidth - current_length
     
-                gaps = len(current_words) - 1
+#                 gaps = len(current_words) - 1
 
 
     
-                if gaps == 0:
+#                 if gaps == 0:
     
-                    # Only one word in this line
+#                     # Only one word in this line
     
-                    res.append(current_words[0] + ' ' * (maxWidth - len(current_words[0])))
+#                     res.append(current_words[0] + ' ' * (maxWidth - len(current_words[0])))
     
-                else:
+#                 else:
     
-                    # Distribute spaces as evenly as possible
+#                     # Distribute spaces as evenly as possible
     
-                    space_per_gap, extra = divmod(total_spaces, gaps)
+#                     space_per_gap, extra = divmod(total_spaces, gaps)
     
-                    line = ""
+#                     line = ""
     
-                    for i in range(gaps):
+#                     for i in range(gaps):
     
-                        line += current_words[i]
+#                         line += current_words[i]
     
-                        # add the base number of spaces
+#                         # add the base number of spaces
     
-                        line += ' ' * space_per_gap
+#                         line += ' ' * space_per_gap
     
-                        # add one more space to the first 'extra' gaps
+#                         # add one more space to the first 'extra' gaps
     
-                        if i < extra:
+#                         if i < extra:
     
-                            line += ' '
+#                             line += ' '
     
-                    # Add the last word
+#                     # Add the last word
     
-                    line += current_words[-1]
+#                     line += current_words[-1]
     
-                    res.append(line)
+#                     res.append(line)
 
 
     
-                # Reset for the next line
+#                 # Reset for the next line
     
-                current_words = []
+#                 current_words = []
     
-                current_length = 0
+#                 current_length = 0
 
 
     
-            # Add the current word to the line
+#             # Add the current word to the line
     
-            current_words.append(w)
+#             current_words.append(w)
     
-            current_length += len(w)
+#             current_length += len(w)
 
 
     
-        # Handle the last line (left-justified)
+#         # Handle the last line (left-justified)
     
-        if current_words:
+#         if current_words:
     
-            line = ' '.join(current_words)
+#             line = ' '.join(current_words)
     
-            # pad the end with spaces
+#             # pad the end with spaces
     
-            line += ' ' * (maxWidth - len(line))
+#             line += ' ' * (maxWidth - len(line))
     
-            res.append(line)
+#             res.append(line)
 
 
     
-        return res
+#         return res
+
+
+class Solution:
+    def mySqrt(self, x: int) -> int:
+        if x == 0:
+            return 0
+        first, last = 1, x
+        while first <= last:
+            mid = first + (last - first) // 2
+            if mid == x // mid:
+                return mid
+            elif mid > x // mid:
+                last = mid - 1
+            else:
+                first = mid + 1
+        return last
