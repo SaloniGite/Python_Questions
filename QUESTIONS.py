803900f4d85659d60eeaa41529886a2aa42a0fe9
SaloniGite <salonigite2003@gmail.com>
2025-03-13 23:43:23 +0530

    questions

--- QUESTIONS.py
+++ QUESTIONS.py
@@ -2226,17 +2226,32 @@
 #         return res
 
 
-class Solution:
-    def mySqrt(self, x: int) -> int:
-        if x == 0:
-            return 0
-        first, last = 1, x
-        while first <= last:
-            mid = first + (last - first) // 2
-            if mid == x // mid:
-                return mid
-            elif mid > x // mid:
-                last = mid - 1
+# class Solution:
+#     def mySqrt(self, x: int) -> int:
+#         if x == 0:
+#             return 0
+#         first, last = 1, x
+#         while first <= last:
+#             mid = first + (last - first) // 2
+#             if mid == x // mid:
+#                 return mid
+#             elif mid > x // mid:
+#                 last = mid - 1
+#             else:
+#                 first = mid + 1
+#         return last
+
+class Solution(object):
+    def merge(self, nums1, m, nums2, n):
+        i = m - 1
+        j = n - 1
+        k = m + n - 1
+        
+        while j >= 0:
+            if i >= 0 and nums1[i] > nums2[j]:
+                nums1[k] = nums1[i]
+                i -= 1
             else:
-                first = mid + 1
-        return last
+                nums1[k] = nums2[j]
+                j -= 1
+            k -= 1
