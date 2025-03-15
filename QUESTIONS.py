dbe68712223965b49397ce070ea62f620f4fd01a
SaloniGite <salonigite2003@gmail.com>
2025-03-15 23:13:45 +0530

    questions

--- QUESTIONS.py
+++ QUESTIONS.py
@@ -2241,17 +2241,41 @@
 #                 first = mid + 1
 #         return last
 
-class Solution(object):
-    def merge(self, nums1, m, nums2, n):
-        i = m - 1
-        j = n - 1
-        k = m + n - 1
+# class Solution(object):
+#     def merge(self, nums1, m, nums2, n):
+#         i = m - 1
+#         j = n - 1
+#         k = m + n - 1
         
-        while j >= 0:
-            if i >= 0 and nums1[i] > nums2[j]:
-                nums1[k] = nums1[i]
-                i -= 1
-            else:
-                nums1[k] = nums2[j]
-                j -= 1
-            k -= 1
+#         while j >= 0:
+#             if i >= 0 and nums1[i] > nums2[j]:
+#                 nums1[k] = nums1[i]
+#                 i -= 1
+#             else:
+#                 nums1[k] = nums2[j]
+#                 j -= 1
+#             k -= 1
+
+
+class Solution:
+    def isScramble(self,s1, s2):
+        m ={}
+        def func(s1, s2):
+            if (s1, s2) in m:
+                return m[(s1, s2)]
+            if not sorted(s1) == sorted(s2):
+                return False
+            if len(s1) == 1:
+                return True
+            
+
+            for i in range(1, len(s1)):
+                if func(s1[:i], s2[-i:]) and func(s1[i:], s2[:-i]) or func(s1[:i], s2[:i]) and func(s1[i:], s2[i:]):
+                    m[(s1, s2)] = True
+                    return True
+            m[(s1, s2)] = False
+            return False
+        return func(s1, s2)
+
+
+
