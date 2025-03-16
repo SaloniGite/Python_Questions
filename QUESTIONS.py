f09ebc35e9ce0a69281d09c716b1a9f6ac36e744
SaloniGite <salonigite2003@gmail.com>
2025-03-16 23:39:39 +0530

    questions

--- QUESTIONS.py
+++ QUESTIONS.py
@@ -2257,25 +2257,38 @@
 #             k -= 1
 
 
-class Solution:
-    def isScramble(self,s1, s2):
-        m ={}
-        def func(s1, s2):
-            if (s1, s2) in m:
-                return m[(s1, s2)]
-            if not sorted(s1) == sorted(s2):
-                return False
-            if len(s1) == 1:
-                return True
+# class Solution:
+#     def isScramble(self,s1, s2):
+#         m ={}
+#         def func(s1, s2):
+#             if (s1, s2) in m:
+#                 return m[(s1, s2)]
+#             if not sorted(s1) == sorted(s2):
+#                 return False
+#             if len(s1) == 1:
+#                 return True
             
 
-            for i in range(1, len(s1)):
-                if func(s1[:i], s2[-i:]) and func(s1[i:], s2[:-i]) or func(s1[:i], s2[:i]) and func(s1[i:], s2[i:]):
-                    m[(s1, s2)] = True
-                    return True
-            m[(s1, s2)] = False
-            return False
-        return func(s1, s2)
+#             for i in range(1, len(s1)):
+#                 if func(s1[:i], s2[-i:]) and func(s1[i:], s2[:-i]) or func(s1[:i], s2[:i]) and func(s1[i:], s2[i:]):
+#                     m[(s1, s2)] = True
+#                     return True
+#             m[(s1, s2)] = False
+#             return False
+#         return func(s1, s2)
 
 
 
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        dp=[-1]*(n+2)
+        def solve(i):
+            if i==0 or i==1:
+                return 1
+            if dp[i]!=-1:
+                return dp[i]
+            left=solve(i-1)
+            right=solve(i-2)
+            dp[i]=left+right
+            return left+right
+        return solve(n)
