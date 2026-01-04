b1acd5829f760e844cb6fa1cd21e5a52eed4f828
SaloniGite <salonigite2003@gmail.com>
2026-01-04 23:00:08 +0530

    m

--- QUESTIONS.py
+++ QUESTIONS.py
@@ -2466,3 +2466,60 @@
 #             curr = curr.next
 #             k -= 1
 #         return curr
+
+
+# def inorderTraversal(root):
+#     result = []
+
+#     def inorder(node):
+#         if not node:
+#             return
+#         inorder(node.left)
+#         result.append(node.val)
+#         inorder(node.right)
+
+#     inorder(root)
+#     return result
+
+# def maxDepth(root):
+#     if not root:
+#         return 0
+#     return 1 + max(maxDepth(root.left), maxDepth(root.right))
+
+
+# def isSameTree(p, q):
+#     if not p and not q:
+#         return True
+#     if not p or not q:
+#         return False
+#     if p.val != q.val:
+#         return False
+
+#     return isSameTree(p.left, q.left) and isSameTree(p.right, q.right)
+
+
+
+# from collections import deque
+
+# def levelOrder(root):
+#     if not root:
+#         return []
+
+#     result = []
+#     queue = deque([root])
+
+#     while queue:
+#         level = []
+#         for _ in range(len(queue)):
+#             node = queue.popleft()
+#             level.append(node.val)
+
+#             if node.left:
+#                 queue.append(node.left)
+#             if node.right:
+#                 queue.append(node.right)
+
+#         result.append(level)
+
+#     return result
+
