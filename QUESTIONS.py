f64a70b619c73b514e2390cb2dadcf848e36c81d
SaloniGite <salonigite2003@gmail.com>
2026-01-06 23:22:42 +0530

    m

--- QUESTIONS.py
+++ QUESTIONS.py
@@ -2549,3 +2549,13 @@
 
 #     postorder(root)
 #     return result
+
+# def sumOfLeftLeaves(root):
+#     if not root:
+#         return 0
+
+#     total = 0
+#     if root.left and not root.left.left and not root.left.right:
+#         total += root.left.val
+
+#     return total + sumOfLeftLeaves(root.left) + sumOfLeftLeaves(root.right)
