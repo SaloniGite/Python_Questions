799843c6460ac170902763897deba97b554b32c2
SaloniGite <salonigite2003@gmail.com>
2026-01-14 23:44:45 +0530

    commit

--- QUESTIONS.py
+++ QUESTIONS.py
@@ -2720,3 +2720,11 @@ def binaryTreePaths(root):
 #     ancestor = lca(root)
 #     return depth(ancestor, p, 0) + depth(ancestor, q, 0)
 
+# def countLeaves(root):
+#     if not root:
+#         return 0
+
+#     if not root.left and not root.right:
+#         return 1
+
+#     return countLeaves(root.left) + countLeaves(root.right)
