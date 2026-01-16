ba3cd97ea5dfd36f48a88064dfb7aa09767ee53d
SaloniGite <salonigite2003@gmail.com>
2026-01-16 23:33:58 +0530

    commit

--- QUESTIONS.py
+++ QUESTIONS.py
@@ -2747,3 +2747,13 @@ def binaryTreePaths(root):
 
 #     _, result = dfs(root)
 #     return result
+
+
+def countLeaves(root):
+    if not root:
+        return 0
+
+    if not root.left and not root.right:
+        return 1
+
+    return countLeaves(root.left) + countLeaves(root.right)
